e8d514f83b9be55f8a12d60f58d7ffd2a7281975
梁德锋 <defeng.liang@3glasses.com>
2020-03-05 17:46:55 +0800

    梯度下降算法，运算theta

--- machine-learning-ex1/ex1/gradientDescent.py
+++ machine-learning-ex1/ex1/gradientDescent.py
@@ -3,16 +3,24 @@ from computeCost import *
 
 
 def gradient_descent(X, y, theta, alpha, num_iters):
+    # gradientDescent Performs gradient descent to learn theta
+    #    theta = gradientDescent(X, y, theta, alpha, num_iters)  updates theta by
+    #    taking num_iters gradient steps with learning rate alpha
+
     # Initialize some useful values
-    m = y.size
+    m = y.size  # number of training examples
     J_history = np.zeros(num_iters)
 
     for i in range(0, num_iters):
         # ===================== Your Code Here =====================
         # Instructions : Perform a single gradient step on the parameter vector theta
         #
-        # Hint: X.shape = (97, 2), y.shape = (97, ), theta.shape = (2, )
+        # Hint: While debugging, it can be useful to print out the values
+        #       of the cost function (computeCost) and gradient here.
+        #       X.shape = (97, 2), y.shape = (97, ), theta.shape = (2, )
 
+        theta = theta - alpha * ((X @ theta - y).T @ X).T / m
+        # theta = theta - alpha * (X.T @ (X @ theta - y)) / m
 
         # ===========================================================
         # Save the cost every iteration
@@ -37,4 +45,3 @@ def gradient_descent_multi(X, y, theta, alpha, num_iters):
         J_history[i] = compute_cost(X, y, theta)
 
     return theta, J_history
-    
