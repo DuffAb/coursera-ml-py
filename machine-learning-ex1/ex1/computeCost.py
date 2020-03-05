b6909cd7ed6e69cf48b10adf69e4ca9a026a16c2
梁德锋 <defeng.liang@3glasses.com>
2020-03-05 17:44:46 +0800

    实现 cost function

--- machine-learning-ex1/ex1/computeCost.py
+++ machine-learning-ex1/ex1/computeCost.py
@@ -3,14 +3,18 @@ import numpy as np
 
 def compute_cost(X, y, theta):
     # Initialize some useful values
-    m = y.size
+    m = y.size  # 代表训练集中实例的数量
     cost = 0
 
     # ===================== Your Code Here =====================
     # Instructions : Compute the cost of a particular choice of theta.
     #                You should set the variable "cost" to the correct value.
-    
+    # print(X.shape)
+    # print(theta.shape)
+    # print(y.shape)
 
+    # cost = sum(np.power((X @ theta - y), 2))/(2*m) # 正确
+    cost = sum((X @ theta - y)**2) / (2 * m)         # 正确
     # ==========================================================
 
     return cost
